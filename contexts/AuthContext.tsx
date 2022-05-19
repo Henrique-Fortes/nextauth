764cbb1b5c97eb457b0f402d8b149d52d5f75243
Henrique Fortes <henrique.fortes@havan.com.br>
2022-05-19 10:50:26 -0300

    Configurado axios

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
 	email: string;
@@ -19,8 +20,17 @@ export const AuthContext = createContext({} as AuthContextData);
 export function AuthProvider({ children }: AuthProviderProps) {
 	const isAuthenticated = false;
 
-	async function signIn({ email, password }: SignInCredentials) {
-		console.log({ email, password });
+	async function signIn({ email, password }: SignInCredentials) {//Sempre colocar try & catch quando for requisicoes de api
+		try {
+			const response = await api.post('sessions', {
+				email,
+				password,
+			})
+
+			console.log(response.data)
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	return (
