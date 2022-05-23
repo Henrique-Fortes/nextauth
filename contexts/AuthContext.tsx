d5259e20baf6e2c8b4bcbf4d809f32ac091eb61f
Henrique Fortes <henrique.fortes@havan.com.br>
2022-05-23 13:58:59 -0300

    Validando visitante

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+	destroyCookie(undefined, 'nextauth.token')
+	destroyCookie(undefined, 'nextauth.refreshToken')
+
+	Router.push('/')
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
 	const [user, setUser] = useState<User>()
 	const isAuthenticated = !!user;
@@ -40,10 +47,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 				setUser({ email, permissions, roles })
 			})
 				.catch(() => {
-					destroyCookie(undefined, 'nextauth.token')
-					destroyCookie(undefined, 'nextauth.refreshToken')
-
-					Router.push('/')
+					signOut()
 				})
 		}
 	}, [])
--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
 
 interface AxiosErrorResponse {
 	code?: string;
@@ -77,8 +78,8 @@ api.interceptors.response.use(
 					});
 				});
 			} else {
-				//deslogar o usuario
+				signOut()
 			}
 		}
-	}
-);
+		return Promise.reject(error);
+	});
