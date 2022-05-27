e284727b7302d3771d07164ad16b70c4ca204d3c
Henrique Fortes <henrique.fortes@havan.com.br>
2022-05-27 14:41:30 -0300

    Criado componente de permissao

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -10,9 +11,7 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
 	const { user } = useContext(AuthContext);
 
-	const userCanSeeMetrics = useCan({
-		permissions: ['administrador', 'editor']
-	})
+
 
 	useEffect(() => {
 		api.get("/me").then((response) => console.log(response));
@@ -21,7 +20,10 @@ export default function Dashboard() {
 	return (
 		<>
 			<h1>Dashboard: {user?.email}</h1>
-			{userCanSeeMetrics && <div>Metricas</div>}
+
+			<Can permissions={['metrics.list']}>
+				<div>Metricas</div>
+			</Can>
 		</>
 	)
 }
